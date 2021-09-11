93c7827e3a41c208d2082f3e7741dabac38856c8
Jhonathan Salazar <salajhon69@gmail.com>
2021-09-11 14:30:48 -0500

    feat: basic db connection with sql server

---
+++ src/main/resources/query.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_roles;
+
+CREATE TABLE roles (
+    id BIGINT IDENTITY(1,1) NOT NULL,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_roles (
+   user_id BIGINT NOT NULL,
+   role_id BIGINT NOT NULL,
+   PRIMARY KEY (user_id, role_id)
+);
