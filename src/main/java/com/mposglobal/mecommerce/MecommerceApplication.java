76f272b2bb79b88780ac6eb034f6b58fb51ad4b2
Jhonathan Salazar <salajhon69@gmail.com>
2021-09-11 13:36:47 -0500

    feat: project init

---
+++ src/main/java/com/mposglobal/mecommerce/MecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mposglobal.mecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MecommerceApplication.class, args);
+    }
+
+}
